3d0e03b8f6b1cdd78f57798b68ac9056f449a655
lucassmoro <lucas.sm.souza@gmail.com>
2026-01-14 15:16:34 -0300

    feat: fusion_image file, correct lidar fusion code version and a gitignore file

--- lidar_filtering/src/lidar_fusion.cpp
+++ lidar_filtering/src/lidar_fusion.cpp
@@ -8,6 +8,8 @@
 #include <cv_bridge/cv_bridge.h>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/highgui/highgui.hpp>
+#include <opencv2/opencv.hpp>
+#include "cv_bridge/cv_bridge.h"
 #include <yaml-cpp/yaml.h>
 #include <Eigen/Dense>
 #include <fstream>
@@ -15,29 +17,47 @@
 #include <message_filters/synchronizer.h>
 #include <message_filters/sync_policies/approximate_time.h>
 #include <functional> // sei nao
+#include <stdio.h>
+#include "yolov8_msgs/msg/yolov8_inference.hpp"
+#include "fs_msgs/msg/track_stamped.hpp"
+#include "fs_msgs/msg/cone.hpp"
+#include <vector>
+#include <cmath>
+#include <pcl/segmentation/extract_clusters.h>
+#include <pcl/search/kdtree.h>
+
+#define IMAGE_WIDTH 768
+#define IMAGE_HEIGHT 480
+
+struct Cluster {
+    std::vector<pcl::PointXYZ> points;
+};
 
 using namespace message_filters;
 typedef sync_policies::ApproximateTime<
-    sensor_msgs::msg::PointCloud2, sensor_msgs::msg::Image> MySyncPolicy;
+    sensor_msgs::msg::PointCloud2,
+    yolov8_msgs::msg::Yolov8Inference> MySyncPolicy;
 
 class PointCloudHandler : public rclcpp::Node {
 public:
   PointCloudHandler() : rclcpp::Node("pcl_transform_from_yaml")
-  , sub_pointcloud{this, "lidar_sub"}
-  , sub_image{this, "image_sub"} 
+  , sub_pointcloud{this, "/ouster/points", rmw_qos_profile_sensor_data}
+  , sub_inference{this, "/yolov8/inferenceresult", rmw_qos_profile_sensor_data} 
+
   {
     sync_ = std::make_shared<Synchronizer<MySyncPolicy>>(
-      MySyncPolicy(10), sub_pointcloud, sub_image);
+      MySyncPolicy(1000), sub_pointcloud, sub_inference);
     sync_->registerCallback(
       std::bind(&PointCloudHandler::cloud_callback,
                 this,
                 std::placeholders::_1,
                 std::placeholders::_2));
     pub_pointcloud = this->create_publisher<sensor_msgs::msg::PointCloud2>("lidar_pub", 10);
+    pub_track = this->create_publisher<fs_msgs::msg::TrackStamped>("track_lidar", 10);
 
-    std::string path_intrinsic = "/home/lucasmoro/ws/src/as_amp/lidar_filtering/config/matrix_intrinsic.yaml";  // substitua pelo caminho real
+    std::string path_intrinsic = "/home/ampera/ws/src/as_amp/lidar_filtering/config/matrix_intrinsic.yaml";  // substitua pelo caminho real
     YAML::Node config_intrinsic = YAML::LoadFile(path_intrinsic);
-    std::string path_extrinsinc = "/home/lucasmoro/ws/src/as_amp/lidar_filtering/config/matrix_extrinsic.yaml"; 
+    std::string path_extrinsinc = "/home/ampera/ws/src/as_amp/lidar_filtering/config/matrix_extrinsic.yaml"; 
     YAML::Node config_extrinsic = YAML::LoadFile(path_extrinsinc);
 
     auto rot_data = config_extrinsic["rotation_matrix"]["data"].as<std::vector<float>>();
@@ -64,15 +84,28 @@ public:
     RT = Eigen::Matrix4f::Identity();
     RT.block<3,3>(0,0) = R;
     RT.block<3,1>(0,3) = t;
+    
+    // Corrige a rotação padrão LiDAR → Camera optical frame
+    Eigen::Matrix4f lidar_to_cam_fix;
+    lidar_to_cam_fix <<
+        0, -1,  0, 0,
+        0,  0,  1, 0,
+        1,  0,  0, 0,
+        0,  0,  0, 1;
 
+    // Aplica essa rotação adicional
+    RT = lidar_to_cam_fix * RT;
     camera_matrix = P * R_rect * RT;
 
     RCLCPP_INFO(this->get_logger(), "Transform loaded from YAML.");
+    std::cout<<RT<<std::endl;
   }
 
 private:
     void cloud_callback(const std::shared_ptr<const sensor_msgs::msg::PointCloud2> pointcloud_msg
-                      , const std::shared_ptr<const sensor_msgs::msg::Image> image_msg) {
+                      , const std::shared_ptr<const yolov8_msgs::msg::Yolov8Inference> inference_msg) {
+      
+      int iterador = 0;
       pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_in(new pcl::PointCloud<pcl::PointXYZ>());
       pcl::fromROSMsg(*pointcloud_msg, *cloud_in);
       
@@ -80,36 +113,144 @@ private:
       pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_filt(new pcl::PointCloud<pcl::PointXYZ>()); 
       cloud_filt->header   = cloud_in->header;   // mantém frame_id, stamp
       cloud_filt->is_dense = cloud_in->is_dense; //mantem is_dense
+
+      cv_bridge::CvImagePtr cv_ptr = cv_bridge::toCvCopy(image_msg, "bgr8");        
       
-      RCLCPP_INFO(this->get_logger(), "Imagem: %d x %d", image_msg->width, image_msg->height);
-      for (const auto& pt : cloud_in->points) {
-        Eigen::Vector4f X(pt.x, pt.y, pt.z, 1.0f); // ponto em coordenadas homogêneas
-        Eigen::Vector3f Y = camera_matrix * X;     // aplica P * R_rect * RT
+      pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_final(new pcl::PointCloud<pcl::PointXYZ>());
+      cloud_final->header   = cloud_in->header;   // mantém frame_id, stamp
+      cloud_final->is_dense = cloud_in->is_dense; //mantem is_dense
 
-        float u = Y(0) / Y(2);
-        float v = Y(1) / Y(2);
+      //cv_bridge::CvImagePtr cv_ptr = cv_bridge::toCvCopy(image_msg, "bgr8");  
+      
+      //std::vector<uint8_t> color_bin;                  
+      fs_msgs::msg::TrackStamped track_final;
+      pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_aux(new pcl::PointCloud<pcl::PointXYZ>());;
+    
+      //RCLCPP_INFO(this->get_logger(), "Imagem: %d x %d", image_msg->width, image_msg->height);
+      cloud_final->points.clear();
+      for (const auto& inf : inference_msg->yolov8_inference) {
+        pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_filt(new pcl::PointCloud<pcl::PointXYZ>);
+        pcl::PointXYZ highest_point;
+        bool first = true;
+        fs_msgs::msg::Cone cone;
 
-        if (u >= -static_cast<float>((image_msg->width)/2) && u < static_cast<float>((image_msg->width)/2) && 
-            v >= -static_cast<float>((image_msg->height)/2) && v < static_cast<float>((image_msg->height)/2)) 
-        {
-          cloud_filt->points.push_back(pt); //se estiver dentro dos limites da camera, preenche a pointcloud
+        for (const auto& pt : cloud_in->points) {
+            Eigen::Vector4f X(pt.x, pt.y, pt.z, 1.0f);
+            Eigen::Vector3f Y = camera_matrix * X;
+            if (Y(2) >= 0) continue;
+            float u = Y(0) / Y(2);
+            float v = Y(1) / Y(2);
+
+            if (u >= inf.top && u <= inf.bottom && 
+                v >= inf.left && v <= inf.right) {
+
+                cloud_filt->points.push_back(pt);
+                if (first || pt.z > highest_point.z) {
+                    highest_point = pt;
+                    first = false;
+                }
+            }
         }
+
+        for (const auto& point : cloud_filt->points) {
+          if (point.z >= highest_point.z - 0.02){
+            cloud_final->points.push_back(point);
+            cloud_aux->points.push_back(point);
+          }
+        }
+        cone = clusterize(cloud_aux, inf.class_name);
+        if (cone.color != fs_msgs::msg::Cone::UNKNOWN){
+          track_final.track.push_back(cone);
+        }
+        cloud_aux->points.clear();
       }
-      cloud_filt->width  = static_cast<uint32_t>(cloud_filt->points.size());
-      cloud_filt->height = 1;
-      RCLCPP_INFO(this->get_logger(), "PointCloud recebida com %zu pontos", cloud_filt->points.size());
-      sensor_msgs::msg::PointCloud2 out_msg;
-      pcl::toROSMsg(*cloud_filt, out_msg);
-      out_msg.header = pointcloud_msg->header;
-      pub_pointcloud->publish(out_msg);
-    }
+
+    cloud_final->width  = static_cast<uint32_t>(cloud_final->points.size());
+    cloud_final->height = 1;
+    RCLCPP_INFO(this->get_logger(), "PointCloud recebida com %zu pontos", cloud_final->points.size());
+
+    // CONVERSAO PCL PARA ROS2 POINTCLOUD
+    sensor_msgs::msg::PointCloud2 out_msg;
+    pcl::toROSMsg(*cloud_final, out_msg);
+    out_msg.header = pointcloud_msg->header;
+    pub_pointcloud->publish(out_msg);
+
+    pub_track->publish(track_final);
+    
+    // CONVERSAO OPENCV PRA ROS2 IMAGE
+    // auto image_msg_painted = cv_ptr->toImageMsg();
+    // pub_image->publish(*image_msg_painted);
+  }
+
+  fs_msgs::msg::Cone clusterize(
+    const pcl::PointCloud<pcl::PointXYZ>::Ptr& cloud_aux,
+    const std::string& cone_class)
+  {
+      fs_msgs::msg::Cone cone_out;
+
+      // Se não tem ponto, retorna cone UNKNOWN em (0,0,0)
+      if (cloud_aux->points.size() <= 0) {
+          cone_out.color = fs_msgs::msg::Cone::UNKNOWN;
+          return cone_out;
+      }
+
+      float mx = mediana_coord(cloud_aux, 'x');
+      float my = mediana_coord(cloud_aux, 'y');
+      float mz = mediana_coord(cloud_aux, 'z');
+      // Preenche cone_out
+      cone_out.location.x = mx;
+      cone_out.location.y = my;
+      cone_out.location.z = mz;
+
+      if (mx == 0.0 || my == 0.0 || mz == 0.0){
+        cone_out.color = fs_msgs::msg::Cone::UNKNOWN;
+        return cone_out;
+      }
+
+      if (cone_class == "yellow_cone")
+          cone_out.color = fs_msgs::msg::Cone::YELLOW;
+      else if (cone_class == "blue_cone")
+          cone_out.color = fs_msgs::msg::Cone::BLUE;
+      else
+          cone_out.color = fs_msgs::msg::Cone::UNKNOWN;
+
+      return cone_out;
+  }
+
+  double mediana_coord(const pcl::PointCloud<pcl::PointXYZ>::Ptr& cloud, char coord) {
+      std::vector<float> vals;
+      vals.reserve(cloud->size());
+
+      for (const auto& p : cloud->points) {
+          switch (coord) {
+              case 'x': vals.push_back(p.x); break;
+              case 'y': vals.push_back(p.y); break;
+              case 'z': vals.push_back(p.z); break;
+              default: throw std::runtime_error("coord inválido (use 'x', 'y' ou 'z')");
+          }
+      }
+
+      if (vals.empty())
+          return 0.0;  // ou trate como quiser
+
+      std::sort(vals.begin(), vals.end());
+
+      int n = vals.size();
+      if (n % 2 == 1) {
+          return vals[n / 2];
+      } else {
+          return (vals[n/2 - 1] + vals[n/2]) / 2.0;
+      }
+  }
 
   Eigen::Matrix4f RT;
   Eigen::Matrix<float, 3, 4> camera_matrix; 
   message_filters::Subscriber<sensor_msgs::msg::PointCloud2> sub_pointcloud;
-  message_filters::Subscriber<sensor_msgs::msg::Image>sub_image;
+  message_filters::Subscriber<yolov8_msgs::msg::Yolov8Inference> sub_inference;
   std::shared_ptr<Synchronizer<MySyncPolicy>> sync_;
   rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr pub_pointcloud;
+  rclcpp::Publisher<fs_msgs::msg::TrackStamped>::SharedPtr pub_track;
+  //rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr pub_image;
 };
 
 int main(int argc, char** argv) {
@@ -120,3 +261,15 @@ int main(int argc, char** argv) {
   return 0;
 }
 
+// if (u >= inf.top + (inf.bottom-inf.top)/3 && u <= inf.bottom - (inf.bottom-inf.top)/3  && 
+//           v >= inf.left + (inf.right - inf.left)/2 && v <= inf.right) 
+//136
+
+// cone.position.x = highest_point.x;
+//     cone.position.y = highest_point.y;
+//     cone.position.z = highest_point.z;
+//     cone.color = (inf.class_name == "yellow_cone")
+//                    ? fs_msgs::msg::Cone::YELLOW
+//                    : fs_msgs::msg::Cone::BLUE;
+
+//     track.track.push_back(cone);
