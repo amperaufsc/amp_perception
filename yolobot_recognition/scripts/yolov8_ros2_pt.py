a7d9de7b086340c53b5da89b09f3cb64213d35ec
OtavioGoulartt <otaviopgoulart06@gmail.com>
2026-01-21 10:48:12 -0300

    fix: correcting code pattern

--- yolobot_recognition/scripts/yolov8_ros2_pt.py
+++ yolobot_recognition/scripts/yolov8_ros2_pt.py
@@ -21,7 +21,7 @@ class Camera_subscriber(Node):
 
         self.subscription = self.create_subscription(
             Image,
-            '/fsds/cam2/image_color',
+            'image',
             self.camera_callback,  
             10)
         self.subscription 
