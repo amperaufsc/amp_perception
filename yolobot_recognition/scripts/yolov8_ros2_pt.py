264aa29d7bd6aeaefa22edd127c82f00339f55a3
OtavioGoulartt <otaviopgoulart06@gmail.com>
2026-05-27 19:32:47 -0300

    fix: added "detected" to yolo model.

--- yolobot_recognition/scripts/yolov8_ros2_pt.py
+++ yolobot_recognition/scripts/yolov8_ros2_pt.py
@@ -21,7 +21,7 @@ class Camera_subscriber(Node):
 
         self.subscription = self.create_subscription(
             Image,
-            '/image_rect/left',
+            '/patinho/left/image_rect',
             self.camera_callback,
             10)
         self.subscription 
@@ -30,16 +30,16 @@ class Camera_subscriber(Node):
         self.img_pub = self.create_publisher(Image, "inferenceimg", 1)
 
         # self.declare_parameter('yolov8_path', 'src/amp_perception/yolobot_recognition/scripts/best.pt')
-        self.declare_parameter('yolov8_path', 'src/amp_perception/yolobot_recognition/scripts/best_nano.pt')
+        self.declare_parameter('yolov8_path', 'src/amp_perception/yolobot_recognition/scripts/teste_19_05/best.pt')
 
         self.yolov8_path = self.get_parameter('yolov8_path').value
 
-        self.declare_parameter('confidence_threshold', 0.6)
+        self.declare_parameter('confidence_threshold', 0.55)
         self.confidence_threshold = self.get_parameter('confidence_threshold').value
 
         self.get_logger().info(f"{self.confidence_threshold}")
 
-        self.model = YOLO(self.yolov8_path)
+        self.model = YOLO(self.yolov8_path, task='detect')
 
     def camera_callback(self, data):
 
