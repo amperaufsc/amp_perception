78811ee10ee0c13246adf0fcbda9730cf41a9b87
OtavioGoulartt <otaviopgoulart06@gmail.com>
2026-06-25 09:09:52 -0300

    fix: add perception lifecycle node.

--- perception/lifecycles/perception_lifecycle_node.py
+++ perception/lifecycles/perception_lifecycle_node.py
@@ -13,35 +13,23 @@ import yaml
 
 from position_estimation.disparity_estimator import DisparityEstimator
 
-class Lifecycle_Perception(Node):
+class Lifecycle_Perception(LifecycleNode):
     def __init__(self):
-        super().__init__('lifecycle_skeleton_node')
+        super().__init__('perception_lifecycle_node')
         self.get_logger().info("Init done: Perception Unconfigured")
 
-    # ==========================================
-    # Callbacks de Transição de Estado
-    # ==========================================
-
     def on_configure(self, state: State) -> TransitionCallbackReturn:
-        """
-        Transição: Unconfigured -> Inactive
-        Objetivo: Ler parâmetros, alocar memória, criar publishers/subscribers/timers.
-        """
         try:
-            
             self.get_logger().info("Configuring Perception")
             
             ## Cria os subscribers
-            self.img_l_msg = Subscriber(self, Image, "camera/left")
-            self.img_R_msg = Subscriber(self, Image, "camera/right")
-            self.disparity_msg = Subscriber(self, Image, "disparity")
+            self.img_l_msg = Subscriber(self, Image, "/oak/left/image_raw")
+            self.img_R_msg = Subscriber(self, Image, "/oak/right/image_raw")
+            self.disparity_msg = Subscriber(self, Image, "/oak/stereo/image_raw")
             self.inference = Subscriber(self, InferenceResult, "inference")
 
             # Cria os lifecycle publishers
             self.track = self.create_lifecycle_publisher(Track, "track", 10)
-            self.emergency_msg = self.create_lifecycle_publisher(String, "/AMP/as_status_indicator", 10)
-            self.transition_pub_emergency = self.create_publisher(TransitionEvent, "EbsNOTMissionFinished", 10)
-
 
             # Declara todos os parâmetros utilizados.
             self.declare_parameter("left_camera_info", "src/amp_perception/perception/config/OAKDLR_left.yaml")
@@ -64,16 +52,11 @@ class Lifecycle_Perception(Node):
 
             return TransitionCallbackReturn.SUCCESS
         
-        except Exception as error:
-            self.get_logger().warn(f"Could not configure Perception! Error : {error}")
+        except Exception as e:
+            self.get_logger().warn(f"Configuration failed: {e}")
             return TransitionCallbackReturn.ERROR
 
-    def on_activate(self, state: State) -> TransitionCallbackReturn:
-        """
-        Transição: Inactive -> Active
-        Objetivo: Ligar efetivamente o sistema (começar a publicar, ativar hardwares).
-        """
-        
+    def on_activate(self, state: State) -> TransitionCallbackReturn:        
         try:
             self.get_logger().info("Activating Perception")
 
@@ -94,21 +77,21 @@ class Lifecycle_Perception(Node):
             
             return TransitionCallbackReturn.SUCCESS
         
-        except Exception as error:
-            self.get_logger().warn(f"Could not activate Perception! Error : {error}")
+        except Exception as e:
+            self.get_logger().warn(f"Activation failed: {e}")
             return TransitionCallbackReturn.ERROR
         
     def on_shutdown(self, state: State) -> TransitionCallbackReturn:
-        """
-        Transição: Qualquer Estado -> Finalized
-        Objetivo: Limpeza absoluta antes do processo ser encerrado de vez.
-        """
-        self.get_logger().info("Exiting node with safety")
+        try:    
+            self.get_logger().info("Exiting node with safety")
+            
+            # TODO: Garantir que portas seriais ou conexões de rede sejam fechadas    
+            return TransitionCallbackReturn.SUCCESS
         
-        # TODO: Garantir que portas seriais ou conexões de rede sejam fechadas
-        
-        return TransitionCallbackReturn.SUCCESS
-    
+        except Exception as e:
+            self.get_logger().warn(f"Shutdown failed: {e}")
+            return TransitionCallbackReturn.ERROR
+
     def callback(self, img_left_msg, img_right_msg, disparity, inference):
         try:
             # Parametros
@@ -122,17 +105,11 @@ class Lifecycle_Perception(Node):
             track = self.perception_methods.Track_Stamped_With_Covariance_Msg_Compose(track, self.img_l_msg.header)
 
             # Publica a track
-            self.emergency_msg.publish("Tadeboa state")
             self.track.publish(track)
 
-        except Exception as error:
-            event = TransitionEvent()
-            self.get_logger().warn(f"Error in callback: {error}")
-            self.get_logger().warn("Emergency state")
-            self.emergency_msg.publish("Emergency state")
-            self.transition_pub_emergency.publish(event)
-            sys.exit(1)
-
+        except Exception as e:
+            self.get_logger().warn(f"Shutdown failed: {e}")
+            return TransitionCallbackReturn.ERROR
 
 def main(args=None):
     rclpy.init(args=args)
