e96082e93727ab96cb395537638c98a2f5343ced
FiremelonX8 <twitteranotherbrick@gmail.com>
2026-07-05 00:30:17 -0300

    fix: order threshold to new value regarding new metrics obtained

--- yolobot_recognition/launch/launch_yolov8.launch.py
+++ yolobot_recognition/launch/launch_yolov8.launch.py
@@ -3,13 +3,18 @@ from launch_ros.actions import Node
 from launch.actions import DeclareLaunchArgument as LaunchArg
 from launch.substitutions import LaunchConfiguration
 
+# TODO: Study how to change an argument in ROS2
+MODEL_NAME = "best.pt"
+
 def generate_launch_description():
 
-    confidence = 0.7
-    yolov8_path = 'src/amp_perception/yolobot_recognition/scripts/best.pt'
+    # Confidene set according to Precision-Recall graph gotten from yolo training
+    # TODO: establish with more accuracy the good confidence
+    confidence = 0.5
+    yolo_path = f'src/amp_perception/yolobot_recognition/scripts/{MODEL_NAME}'
 
     return LaunchDescription([
-        LaunchArg('namespace', default_value=['yolov8'], description='Namespace for node'),
+        LaunchArg('namespace', default_value=[''], description='Namespace for node'),
         LaunchArg('image',default_value=['image'],description='img topic'),
         LaunchArg('inferenceresult',default_value=['inferenceresult'],description='bounding box coordenates on image'),
         LaunchArg('inferenceimg',default_value=['inferenceimg'],description='img with boundingbox'),
@@ -26,7 +31,7 @@ def generate_launch_description():
                 ],
             parameters=[
             {'confidence_threshold': confidence},
-            {'yolov8_path': yolov8_path}
+            {'yolo_path': yolo_path}
         ]
     )
     ])
